f5b433665e10842f4a27ca6f56816522049255c5
Jonathan <jonathanmatheus1997.jm@gmail.com>
2018-06-23 16:32:58 -0300

    Consumindo API do instAlura

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './css/reset.css';
+import './css/timeline.css';
+import './css/login.css'
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
